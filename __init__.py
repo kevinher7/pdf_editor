ac2a258d9b131550b66c12c2924406970aacda66
Kevin Hernandez <kevinhernem@gmail.com>
2024-11-30 00:22:17 +0900

    fix: resolve issues to run project as an external command

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .utils import get_pdfs_in_path
---
+++ pdfmerge/pdfmerge.py
@@ -0,0 +1,28 @@
+# __package__ = 'pdf_editor.pdfmerge'
+
+import os
+
+from PyPDF2 import PdfWriter
+
+from ..utils import get_pdfs_in_path
+
+def main():
+    current_path = os.getcwd()
+    pdf_files = get_pdfs_in_path(current_path)
+
+    merger = PdfWriter()
+
+    for pdf in pdf_files:
+        merger.append(pdf)
+
+    os.makedirs("./output", exist_ok=True) 
+    merger.write("./output/merged.pdf")
+    merger.close()
+
+    print(f"Merged {len(pdf_files)} files!")
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
