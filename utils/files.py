54c2169d3728f2bb51250bb74ce9ba2f9b2ab91a
Kevin Hernandez <kevinhernem@gmail.com>
2024-11-30 15:10:18 +0900

    chore: add util to ensure the file extension is .pdf

--- utils/files.py
+++ utils/files.py
@@ -8,3 +8,9 @@ def get_pdfs_in_path(path):
             pdf_paths_list.append(file)
 
     return pdf_paths_list
+
+def ensure_pdf_extension(file_name):
+    if file_name[-4:] != ".pdf":
+        file_name += ".pdf"
+
+    return file_name
