a4a54064c3e09213b2b7bd4a120732be18617010
Kevin Hernandez <kevinhernem@gmail.com>
2024-11-30 14:32:38 +0900

    refactor: remove ghostscript helper file and add logic to pdfcompress module

--- pdfcompress/pdfcompress.py
+++ pdfcompress/pdfcompress.py
@@ -1,7 +1,11 @@
 import os
+import subprocess
 import argparse
 
-from ..utils import compress_pdf
+def compress_pdf(input_path, output_path, quality):
+    subprocess.call(["gswin64c", "-sDEVICE=pdfwrite", "-dCompatibilityLevel=1.4",
+                     f"-dPDFSETTINGS=/{quality}", "-dNOPAUSE", "-dQUIET", "-dBATCH",
+                     f"-sOutputFile={output_path}", input_path], shell=True)
 
 def main():
     parser = argparse.ArgumentParser(description="Compress PDF file")
@@ -12,7 +16,7 @@ def main():
     current_path = os.getcwd()
 
     # compress_pdf(args.input_file, args.output_file, "default")
-    compress_pdf("C:/Users/kevin/Downloads/output/merged.pdf", "C:/Users/kevin/Downloads/output/compressed.pdf", "default")
+    compress_pdf("C:/Users/kevin/Downloads/output/2273141s_soryuusi5.pdf", "C:/Users/kevin/Downloads/output/compressed.pdf", "default")
 
 
 if __name__ == "__main__":
--- utils/ghostscript.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-def compress_pdf(input_path, output_path, quality):
-    subprocess.call(["gswin64c", "-sDEVICE=pdfwrite", "-dCompatibilityLevel=1.4",
-                     f"-dPDFSETTINGS=/{quality}", "-dNOPAUSE", "-dQUIET", "-dBATCH",
-                     f"-sOutputFile={output_path}", input_path], shell=True)
-
