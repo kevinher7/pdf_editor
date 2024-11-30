c279fb87d72d214713b47d9bc27f1d6068c303dc
Kevin Hernandez <kevinhernem@gmail.com>
2024-11-30 15:39:11 +0900

    chore: add print to console on compress success

--- pdfcompress/pdfcompress.py
+++ pdfcompress/pdfcompress.py
@@ -21,6 +21,8 @@ def compress_pdf(input_file, output_file=None, quality="screen"):
     subprocess.call(["gswin64c", "-sDEVICE=pdfwrite", "-dCompatibilityLevel=1.4",
                      f"-dPDFSETTINGS=/{quality}", "-dNOPAUSE", "-dQUIET", "-dBATCH",
                      f"-sOutputFile={output_path}", input_path], shell=True)
+    
+    print(f"Sucessfully compressed File into {output_file}")
 
 def main():
     parser = argparse.ArgumentParser(description="Compress PDF file")
