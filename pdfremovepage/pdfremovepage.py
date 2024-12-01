da2b8692b9d7a7dbc8d65dc171e87a9a6ffe96e3
Kevin Hernandez <kevinhernem@gmail.com>
2024-12-02 02:27:28 +0900

    feat: add functionality to remove a given page from a PDF

---
+++ pdfremovepage/pdfremovepage.py
@@ -0,0 +1,48 @@
+import argparse
+
+from PyPDF2 import PdfWriter, PdfReader
+
+from ..utils import get_pdf_path_from_name
+
+def get_pdf_pages(file_name):
+    file_path = get_pdf_path_from_name(file_name)
+    input_pdf = PdfReader(file_path)
+
+    input_pdf_pages = input_pdf.pages
+
+    return input_pdf_pages
+
+def remove_page(file_name, page_number_to_remove):
+    merger = PdfWriter()
+
+    pdf_pages = list(get_pdf_pages(file_name))
+
+    pdf_pages.pop(page_number_to_remove - 1)
+
+    for page in pdf_pages:
+        merger.add_page(page)
+
+    merger.write(f"./{file_name}_r{page_number_to_remove}.pdf")
+    merger.close()
+
+    print(f"Removed page number {page_number_to_remove} from {file_name}.pdf")
+
+    return 0
+
+def main():
+    parser = argparse.ArgumentParser(description="Remove Page from PDF file")
+    parser.add_argument("input_file", type=str, help="Name of input file (file to compress)")
+    parser.add_argument("page_to_remove", type=int, help="Page to Remove from the PDF")
+    args = parser.parse_args()
+
+
+    if args.input_file is None:
+        raise ValueError("Input file not provided.")
+    
+    if args.page_to_remove is None:
+        raise ValueError("Page to remove not provided.")
+    
+    remove_page(args.input_file, args.page_to_remove)
+
+if __name__ == "__main__":
+    main()
