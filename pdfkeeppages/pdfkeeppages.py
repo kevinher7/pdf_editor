413561543431b39f93dfe647250a362bc0b92a7e
Kevin Hernandez <kevinhernem@gmail.com>
2025-02-21 00:24:01 +0900

    chore: fix typo/help for args and move merger initialization for better performance

--- pdfkeeppages/pdfkeeppages.py
+++ pdfkeeppages/pdfkeeppages.py
@@ -10,23 +10,22 @@ def keep_pages(file_name, pages_range):
     # Get pages range as a list
     pages_range = parse_pages_range(pages_range)
 
-    merger = PdfWriter()
-
     pdf_pages = list(get_pdf_pages(file_name))
 
     if pages_range[1] == "end":
         pages_range[1] = len(pdf_pages)
 
+    merger = PdfWriter()
+
     for page_number in range(pages_range[0], pages_range[1], 1):
         merger.add_page(pdf_pages[page_number])
 
-    merger.write(f"./{file_name}_k{pages_range[0]}-{pages_range[1]}.pdf")
+    merger.write(
+        f"./{file_name}_k{pages_range[0] + 1}-{pages_range[1] + 1}.pdf")
     merger.close()
 
     print(
-        f"Removed pages outside range [{pages_range[0]} - {pages_range[1]}] from {file_name}.pdf")
-
-    return 0
+        f"Removed pages outside range [{pages_range[0] + 1} - {pages_range[1] + 1}] from {file_name}.pdf")
 
 
 def main():
@@ -35,7 +34,7 @@ def main():
     parser.add_argument("input_file", type=str,
                         help="Name of input file (file to compress)")
     parser.add_argument("pages_to_keep", type=str,
-                        help="Page to Remove from the PDF")
+                        help="Pages to keep from the PDF")
     args = parser.parse_args()
 
     if args.input_file is None:
